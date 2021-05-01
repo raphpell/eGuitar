23a75798502508cdd5d08f29954ee4cb334d3d69
raphpell <pellicierraphael@neuf.fr>
2021-05-01 13:36:13 +0200

    tmp

--- js/audio.js
+++ js/audio.js
@@ -76,3 +76,36 @@ playTone =(function(){
 		o.start(0)
 		}
 	})()
+
+
+/* hallucinant
+//compressé
+with(new AudioContext)for(t=i=0;n=parseInt('l43l431db98643o86ogfdbdfdgfdzbdzgigikigfdbzbdv98db9864311480'[i++],36);)	with(createOscillator())frequency.value=880*2**(-~-n%20/12),connect(destination),start(t),stop(i>56?t+q*8:t+=n>20?q=6/13:q/2)
+
+
+// Packed notation string
+var sMusique = 'l43l431db98643o86ogfdbdfdgfdzbdzgigikigfdbzbdv98db9864311480'
+with(new AudioContext)            // use HTML5 audio
+  for(                            // iterate through the note pitches and lengths
+    t=i=0;                        // t = current time to place the note
+    n=parseInt(sMusique[i++],36);                // n = note pitch/length
+    )
+    with(createOscillator())      // create the note oscillator
+
+      // Set the note frequency (using Math.pow for the demo).
+      //frequency.value=880*2**(-~-n%20/12),
+      frequency.value=880*Math.pow(2,-~-n%20/12),
+
+      // Send the note's sound through the speakers (for the demo, we'll connect it to
+      // a gain node so we can reduce the volume).
+      //connect(destination),
+      connect((g=createGain(),g.gain.value=.3,g.connect(destination),g)),
+
+      start(t),                     // schedule the note to sound
+      stop(                         // schedule the end of the note
+        i>56?                       // if we are in the final chord
+          t+                        //   do not increment the time
+            q*8                     //   hard-code the length to a semibreve
+        :t+=n>20?q=6/13:q/2         // else update the length based on the note value
+      )
+ */
